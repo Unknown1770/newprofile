3227e250c0519b835fb853ae1c7441d0a0a5d53f
Vijayant Prateek <62292932+Unknown1770@users.noreply.github.com>
2021-03-16 20:33:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-from flask import Flask
+# importing Flask and other modules 
+from flask import Flask, request, render_template 
 
+# Flask constructor 
+app = Flask(__name__) 
 
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
-app = Flask(__name__)
+# A decorator used to tell the application 
+# which URL is associated function 
+@app.route('/', methods =["GET", "POST"]) 
+def gfg(): 
+	if request.method == "POST": 
+	# getting input with name = fname in HTML form 
+	first_name = request.form.get("fname") 
+	# getting input with name = lname in HTML form 
+	last_name = request.form.get("lname") 
+	return "Your name is "+first_name + last_name 
+	return render_template("form.html") 
 
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
+if __name__=='__main__': 
+app.run() 
