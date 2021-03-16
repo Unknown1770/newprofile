ecc9b4d31200ccbbd244c744215d2200c063339e
Vijayant Prateek <62292932+Unknown1770@users.noreply.github.com>
2021-03-16 21:27:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ def gfg():
 	return "Your name is "+first_name + last_name 
 	return render_template("form.html") 
 
+@app.route('/karthik', methods =["GET", "POST"]) 
+def gfgf(): 
+	if request.method == "POST": 
+	# getting input with name = fname in HTML form 
+	first_name = request.form.get("fname") 
+	return "Your name is "+first_name 
+	return render_template("form.html") 
+
 if __name__=='__main__': 
    #app.run()
    app.run(host='127.0.0.1', port=8080, debug=True)
